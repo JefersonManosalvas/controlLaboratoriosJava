67360893eb35522add4a7ea54d9f6318a96117dc
JefersonManosalvas <132539368+JefersonManosalvas@users.noreply.github.com>
2023-07-12 12:25:34 -0500

    inicializar git en java

---
+++ src/main/java/prvisual/controllaboratorios/ControlLaboratorios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prvisual.controllaboratorios;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class ControlLaboratorios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
