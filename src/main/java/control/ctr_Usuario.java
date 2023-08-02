18375a58d342e0a8d3e4b5cfc07964d432fa6883
saidscv23 <132930553+saidscv23@users.noreply.github.com>
2023-08-02 09:11:52 -0500

    Segundo commit

--- src/main/java/control/ctr_Usuario.java
+++ src/main/java/control/ctr_Usuario.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 import modelo.Usuario;
 
 /**
@@ -58,6 +59,24 @@ public class ctr_Usuario {
         return respuesta;
     }
 
+    public boolean loginUser(Usuario objeto) {
+        boolean respuesta = false;
+        Connection cn = conexion.conectar();
+        String sql = "select  usuario, password from tb_usuario where usuario = '" + objeto.getUsuario() + "' and password = '" + objeto.getPassword() + "'";
+        Statement st;
+        try {
+            st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                respuesta = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al Iniciar Sesion");
+            JOptionPane.showMessageDialog(null, "Error al Iniciar Sesion");
+        }
+        return respuesta;
+    }
+    
 }
 
 
